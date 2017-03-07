a6675107a179727ec40ca6b8fb195b3846388675
Aaron_Lin <Aaron_Lin@wistron.com>
2017-03-07 15:41:29 +0800

    aa

--- sendQueueMessage/index.js
+++ sendQueueMessage/index.js
@@ -3,6 +3,35 @@
 
     if (req.query.message || (req.body && req.body.message)) {
 
+        var message = (req.query.message || req.body.message);
+
+        var connectString = 'DefaultEndpointsProtocol=https;AccountName=function41bedd119c61;AccountKey=PuHsT4RStNtm4FPSVnyA+5Rl9mtRYDnu4tdacUXqcLfCy6cqmNtlQDvXd1s4Sn9ZtbwpOV4bnEiKta2NHsiBJg==;';
+        var azure = require('azure-storage');
+        var queueName = 'aaronqueue';
+        var queueService = azure.createQueueService(connectString);
+
+        queueService.createQueueIfNotExists(queueName, function (error) {
+            if (!error) {
+                // Queue exists
+            }
+        });
+
+
+        queueService.createMessage(queueName, message, function (error) {
+            if (!error) {
+                console.log('Message inserted');
+            }
+        });
+
+        queueService.getMessages(queueName, function (error, serverMessages) {
+            if (!error) {
+                // Process the message in less than 30 seconds, the message
+                // text is available in serverMessages[0].messageText
+                console.log("1", serverMessages);
+            }
+        });
+
+
         context.res = {
             status: 200,
             body: "Success: " + (req.query.message || req.body.message)
