846767dee30e58f4353f69f4120cffb703a9f6a3
Aaron_Lin <Aaron_Lin@wistron.com>
2017-03-08 17:53:38 +0800

    cc

---
+++ getFace/index.js
@@ -0,0 +1,43 @@
+﻿var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+var config = {
+    userName: 'aaron',
+    password: 'q999999Q',
+    server: 'aaron-db-server.database.windows.net',
+    // If you are on Microsoft Azure, you need this:  
+    options: { encrypt: true, database: 'aaron-db-1' }
+};
+var connection = new Connection(config);
+var dataset = [];
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    connection.on('connect', function (err) {
+        console.log("Connected");
+        request = new Request("SELECT * FROM face_info", function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        var result = "";
+        request.on('row', function (columns) {
+            columns.forEach(function (column) {
+                dataset.push({
+                    col: column.metadata.colName,
+                    val: column.value
+                });
+            });
+        });
+        request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+            context.res = {
+                status: 200,
+                body: JSON.stringify(dataset)
+            };
+            context.done();
+        });
+        connection.execSql(request);
+    });
+
+};
---
+++ insertFace/index.js
@@ -0,0 +1,72 @@
+﻿module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var Connection = require('tedious').Connection;
+    var Request = require('tedious').Request;
+    var TYPES = require('tedious').TYPES;
+
+    var config = {
+        userName: 'aaron',
+        password: 'q999999Q',
+        server: 'aaron-db-server.database.windows.net',
+        // If you are on Microsoft Azure, you need this:  
+        options: { encrypt: true, database: 'aaron-db-1' }
+    };
+    var connection = new Connection(config);
+
+    var faceId = (req.query.faceId || req.body.faceId);
+    var gender = (req.query.gender || req.body.gender);
+    var age = (req.query.age || req.body.age);
+
+    connection.on('connect', function (err) {
+        console.log("Connected");
+
+        request = new Request("INSERT INTO face_info (faceId, gender, age) VALUES (@faceId, @gender, @age)", function (err, rowCount) {
+            if (err) {
+                console.error(err);
+            }
+        });
+
+        request.addParameter('faceId', TYPES.NVarChar, faceId);
+        request.addParameter('gender', TYPES.NVarChar, gender);
+        request.addParameter('age', TYPES.NVarChar, age);
+        /*
+        request.on('row', function (columns) {
+            columns.forEach(function (column) {
+                if (column.value === null) {
+                    console.log('NULL');
+                } else {
+                    console.log("Product id of inserted item is " + column.value);
+                }
+            });
+        });
+        */
+        connection.execSql(request);
+
+        context.res = {
+            status: 200,
+            body: 'Success'
+        };
+
+        context.done();
+    });
+
+
+
+    /*
+        request = new Request("INSERT INTO face_info (faceId, gender, age) VALUES (" + faceId + ", " + gender + ", " + age + ")", function (err) {
+            if (err) {
+                console.log(err);
+                context.res = {
+                    status: 400,
+                    body: err
+                };
+            } else {
+                context.res = {
+                    status: 200,
+                    body: 'Success'
+                };
+            }
+        });
+        */
+};
